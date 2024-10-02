083f1b012eabf0fbc30ecdd54c29d828635f26ed
Salvador Frisco <salvadorfrisco70@gmail.com>
2024-10-02 19:32:45 -0300

    acessando banco remoto

--- src/pages/api/faturas.js
+++ src/pages/api/faturas.js
@@ -1,44 +1,21 @@
-// import excuteQuery from "../../lib/db";
-
-// export default async function handler(req, res) {
-//   if (req.method === "GET") {
-//     try {
-//       const query =
-//         "SELECT id, Data, Estabelecimento, Portador, Valor, Parcela FROM fatura";
-//       const results = await excuteQuery({ query });
-
-//       if (results.error) {
-//         console.log(results.error);
-//         return res.status(500).json({ error: results.error });
-//       }
-
-//       res.status(200).json({ faturas: results });
-//     } catch (error) {
-//       console.log(error);
-//       res.status(500).json({ error: "Ocorreu um erro ao buscar os dados." });
-//     }
-//   } else {
-//     res.setHeader("Allow", ["GET"]);
-//     res.status(405).end(`Método ${req.method} não permitido`);
-//   }
-// }
-
-import axios from "axios";
+import excuteQuery from "../../lib/db";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
-      // Fazendo uma chamada GET para a API externa
-      const response = await axios.get(
-        `${process.env.BASE_URL}/api/api_faturas.php`
-      );
+      const query =
+        "SELECT id, Data, Estabelecimento, Portador, Valor, Parcela FROM fatura";
+      const results = await excuteQuery({ query });
 
-      // Se a resposta for bem-sucedida, enviamos os dados de volta
-      res.status(200).json({ faturas: response.data });
+      if (results.error) {
+        console.log(results.error);
+        return res.status(500).json({ error: results.error });
+      }
+
+      res.status(200).json({ faturas: results });
     } catch (error) {
-      // Tratamento de erro, retornando a mensagem de erro
-      console.error("Erro ao buscar dados da API externa:", error);
-      res.status(500).json({ error: "Erro ao buscar dados da API externa." });
+      console.log(error);
+      res.status(500).json({ error: "Ocorreu um erro ao buscar os dados." });
     }
   } else {
     res.setHeader("Allow", ["GET"]);
